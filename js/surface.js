b70478fa8544e25070744571234ee7b23683aa33
Andrew Borg <andrewbrg87@gmail.com>
2018-08-25 22:51:48 +0200

    Create surface.js

---
+++ js/surface.js
@@ -0,0 +1,8 @@
+class Surface {
+    constructor(color, specular, lambert, ambient) {
+        this.color = color;
+        this.specular = specular;
+        this.lambert  = lambert;
+        this.ambient  = ambient;
+    }
+}
