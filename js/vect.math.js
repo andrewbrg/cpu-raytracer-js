b603228172893e8a82097a65294122bc6cfcb0c8
Andrew Borg <andrewbrg87@gmail.com>
2018-08-26 17:10:53 +0200

    Update vect.math.js

--- js/vect.math.js
+++ js/vect.math.js
@@ -3,23 +3,13 @@ class VectMath {
     static make(type = 'zero') {
         switch(type) {
             case 'zero':
-                return {x: 0, y: 0, z: 0};
+                return new Vector(0, 0, 0);
             case 'up':
-                return {x: 0, y: 1, z: 0};
+                return new Vector(0, 1, 0);
             case 'down':
-                return {x: 0, y: -1, z: 0};
-            case 'left':
-                return {x: -1, y: 0, z: 0};
-            case 'right':
-                return {x: 1, y: 0, z: 0};
-            case 'in':
-                return {x: 0, y: 0, z: 1};
-            case 'out':
-                return {x: 0, y: 0, z: -1};
-            case 'white':
-                return {x: 255, y: 255, z: 255};
+                return new Vector(0, -1, 0);
             default:
-                return {x: 0, y: 0, z: 0};
+                return new Vector(0, 0, 0);
         }
     }
     
@@ -31,43 +21,43 @@ class VectMath {
         return Math.sqrt(this.dot(a, a));
     } 
     
+    static reflect(a, norm) {
+        return this.sub(this.scale(this.scale(norm, this.dot(a, norm)), 2), a);
+    }
+    
     static dot(a, b) {
         return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
     }
     
     static cross(a, b) {
-        return {
-            x: (a.y * b.z) - (a.z * b.y),
-            y: (a.z * b.x) - (a.x * b.z),
-            z: (a.x * b.y) - (a.y * b.x)
-        };
+        return new Vector(
+            (a.y * b.z) - (a.z * b.y), 
+            (a.z * b.x) - (a.x * b.z),
+            (a.x * b.y) - (a.y * b.x)
+        );
     }
     
     static scale(a, s) {
-        return {
-            x: a.x * s,
-            y: a.y * s,
-            z: a.z * s
-        };
+        return new Vector(
+            a.x * s,
+            a.y * s,
+            a.z * s
+        );
     }
     
     static add(a, b) {
-        return {
-            x: a.x + b.x,
-            y: a.y + b.y,
-            z: a.z + b.z
-        };
+        return new Vector(
+           a.x + b.x,
+           a.y + b.y,
+           a.z + b.z
+        );
     }
     
     static sub(a, b) {
-        return {
-            x: a.x - b.x,
-            y: a.y - b.y,
-            z: a.z - b.z
-        };
-    }
-    
-    static reflect(a, norm) {
-        return this.sub(this.scale(this.scale(norm, this.dot(a, norm)), 2), a);
+        return new Vector(
+           a.x - b.x,
+           a.y - b.y,
+           a.z - b.z
+        );
     }
 }
